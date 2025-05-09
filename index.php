b2723136d3795a280f1927afed379678d4547dad
russyncb <108273213+russyncb@users.noreply.github.com>
2025-05-09 10:11:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // Main index file - Entry point to the application
-require_once $_SERVER['DOCUMENT_ROOT'] . '/systems/claude/shasha/bootstrap.php';
+require_once __DIR__ . '/bootstrap.php';
 
 // Redirect to homepage
 header("Location: " . SITE_URL . "/views/common/home.php");
